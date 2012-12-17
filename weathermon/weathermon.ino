f7e34a9b986126c0fcf7924dbdbd3e1b00dcadfa
Gareth Halfacree <gareth@halfacree.co.uk>
2012-12-17 17:00:42 +0000

    Bones of the sketch finished.

--- weathermon/weathermon.ino
+++ weathermon/weathermon.ino
@@ -5,32 +5,34 @@ Requires the DHT library: http://playground.arduino.cc/Main/DHTLib
 Requires the BMP085 library: https://github.com/adafruit/Adafruit-BMP085-Library
 */
 
-#include <dht.h>
+#include "dht.h"
+#include "Wire.h"
+#include "Adafruit_BMP085.h"
+
+Adafruit_BMP085 bmp;
 
 dht DHT;
 #define DHT22_PIN A3
+float realtemp=0;
 
 void setup()
 {
   Serial.begin(9600);
+  bmp.begin();
   pinMode(DHT22_PIN, INPUT);
   digitalWrite(DHT22_PIN, HIGH);  
-  Serial.println("Weather Monitor");
-  Serial.print("---------------");
-  Serial.println(DHT_LIB_VERSION);
+  Serial.println("Weather Monitor Data");
   Serial.println();
-  Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)");
+  Serial.println("Temperature,Relative Humidity,Pressure");
 }
 
 void loop()
 {
-  // READ DATA
-  Serial.print("DHT22, \t");
+
   int chk = DHT.read22(DHT22_PIN);
   switch (chk)
   {
     case DHTLIB_OK:  
-                Serial.print("OK,\t");
                 break;
     case DHTLIB_ERROR_CHECKSUM:
                 Serial.print("Checksum error,\t");
@@ -42,14 +44,12 @@ void loop()
                 Serial.print("Unknown error,\t");
                 break;
   }
-  // DISPLAY DATA
+  
+  realtemp=((DHT.temperature)+(bmp.readTemperature()))/2;
+  Serial.print(realtemp, 1);
+  Serial.print(",");
   Serial.print(DHT.humidity, 1);
-  Serial.print(",\t");
-  Serial.println(DHT.temperature, 1);
-
+  Serial.print(",");
+  Serial.println(bmp.readPressure());
   delay(5000);
 }
-//
-// END OF FILE
-//
-
