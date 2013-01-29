81cb5af3a9a6dcba36479eb1292b976b5a94ed19
Gareth Halfacree <gareth@halfacree.co.uk>
2013-01-29 15:15:54 +0000

    Macro Keyboard skeleton sketch.

---
+++ macro_keyboard/macro_keyboard.ino
@@ -0,0 +1,13 @@
+/*
+   Custom Macro Keyboard
+   Requires Arduino Leonardo or compatible
+   Written by Gareth Halfacree, http://freelance.halfacree.co.uk
+   Based on work by Tom Igoe, http://arduino.cc/en/Tutorial/KeyboardButton
+   Parts list:
+       4x momentary push-buttons: http://www.oomlout.co.uk/pushbuttons-x5-p-196.html
+       Jumper wires: http://www.oomlout.co.uk/breadboard-jumper-wires-70-pieces-p-181.html
+       Breadboard (optional): http://www.oomlout.co.uk/breadboard-400-point-p-180.html
+   Written for PC Pro Magazine, January 2013
+*/
+
+
